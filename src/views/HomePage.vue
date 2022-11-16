c2ab6815285f5f031396842f4505847f85e50d1f
Ssj <2226435102@qq.com>
2022-11-16 17:22:57 +0800

    布局

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,1 +1,44 @@
-<template>首页</template>
+<template>
+    <div class="warp">
+        <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="item in Carousel" :key="item">
+      <img :src="item.src" alt="">
+      {{item.src}}
+    </el-carousel-item>
+  </el-carousel>
+    </div>
+</template>
+<script lang="ts" setup>
+let Carousel = [
+    {
+        id:1,
+        src:"@/assets/images/Carousel-01.png"
+    }, {
+        id:2,
+        src:"@/assets/images/Carousel-02.png"
+    }, {
+        id:3,
+        src:"@/assets/images/Carousel-03.png"
+    }, {
+        id:4,
+        src:"@/assets/images/Carousel-04.png"
+    },
+]
+</script>
+<style>
+.el-carousel__item h3 {
+  display: flex;
+  color: #475669;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,114 @@
 <template>
-    <div class="common-layout">
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>
-         <HomePage></HomePage>
-        </el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
-    </div>
-  </template>
-  <style>
-  
-  </style>
-  <script setup lang="ts">
-import HomePage from './HomePage.vue';
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <div class="warp box-header">
+          <div><img class="logo" src="@/assets/images/icon-wll_logo.png" alt=""></div>
+          <div class="box-rigth">
+            <div class="text"><span>乌拉拉餐饮</span>
+              <p>WULALAFOOD</p>
+            </div>
+
+            <div class="login">
+              <div class="header-fw">服务</div>
+              <div class="header-fw">线下店铺</div>
+              <el-icon class="icon" size="24">
+                <Search />
+              </el-icon>
+              <el-dropdown>
+                <span class="el-dropdown-link">
+                  <el-icon class="icon" size="24">
+                    <User />
+                  </el-icon>
+                </span>
+                <template #dropdown>
+                  <el-dropdown-menu>
+                    <el-dropdown-item>登录</el-dropdown-item>
+                    <el-dropdown-item divided>注册</el-dropdown-item>
+                  </el-dropdown-menu>
+                </template>
+              </el-dropdown>
+              <div class="collection">
+                <div>收藏店铺</div>
+                <el-button type="primary" color="#000000" round size="small" :icon="Edit">点击收藏</el-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </el-header>
+
+      <el-main>
+        <RouterView />
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+<style>
+.logo {
+  width: 60px;
+  height: 60px;
+  margin-left: 25px;
+}
+
+.el-header {
+  height: 80px;
+  border-bottom: 1px solid #ccc;
+}
+
+.box-header {
+  display: flex;
+  align-items: center
+}
+
+.box-rigth {
+  margin: 10px 30px;
+  width: 1080px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.collection {
+  border: 1px solid rgb(80, 80, 80);
+  padding: 5px 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.text {
+  padding: 5px;
+}
+
+.text span {
+  font-size: 20px;
+}
+
+.text p {
+  font-size: 15px;
+}
+
+.login {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  margin: 0 20px 0 0px;
+}
+
+.header-fw {
+  margin-right: 20px;
+}
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
+<script setup lang="ts">
+
+import { ArrowDown } from '@element-plus/icons-vue'
+import { Edit, Search, User } from '@element-plus/icons-vue'
 </script>
