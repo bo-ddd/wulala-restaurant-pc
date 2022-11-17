18d1e51f4b7ba5cea46925b30abfb134af2eb580
hyx1958895296 <1958895296@qq.com>
2022-11-17 08:47:09 +0800

    login&register already ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from "@/views/Layout.vue"
 import Login from "@/views/Login/Login.vue"
-import Registration from "@/views/Login/Registration.vue"
+// import Registration from "@/views/Login/Registration.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,11 +14,7 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login,
-    },{
-      path: '/registration',
-      name: 'registration',
-      component: Registration,
-    },
+    }
 
   ]
 })
