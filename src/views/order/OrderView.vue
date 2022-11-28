c8d31b803a757d8a6cb298cd663119fb1e61d037
lwb <1375229126@qq.com>
2022-11-28 16:04:04 +0800

    呀呼

--- src/views/order/OrderView.vue
+++ src/views/order/OrderView.vue
@@ -42,74 +42,6 @@
                 <el-table-column label="状态" width="90" />
                 <el-table-column label="操作" width="80" />
               </el-table>
-
-              <tbody>
-                <tr class="tr-th">
-                  <td colspan="5" class="td">
-                    <span class="dealtime">2021-11-16 19:36:21</span>
-                    <span class="number">订单号:</span>
-                    <span class="numbers">230307184410</span>
-                    <span class="titless">京耳官方旗舰店</span>
-                    <img
-                      class="del"
-                      src="@/assets/images/icon-del.png"
-                      alt=""
-                    />
-                  </td>
-                </tr>
-                <tr class="tr-bd">
-                  <td>
-                    <div class="goods-item">
-                      <div class="p-img">
-                        <img src="@/assets/images/jer.jpg" alt="" />
-                      </div>
-                      <div class="p-msg">
-                        <div class="p-name">
-                          京耳
-                          蓝牙耳机真无线运动跑步游戏音乐降噪电脑双耳适用智能手机
-                          白色
-                        </div>
-                      </div>
-                    </div>
-                    <div class="goods-number">x1</div>
-                    <div class="goods-repair">
-                      <span class="br">申请售后</span>
-                      <span class="br">卖了换钱</span>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="user">
-                      <span class="txt">刘伟博</span>
-                      <img
-                        class="user-img"
-                        src="@/assets/images/icon-user.png"
-                        alt=""
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div class="amount">
-                      <span>￥9.90</span>
-                      <br />
-                      <span class="ftx-13">在线支付</span>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="status">
-                      <span class="amount">已完成</span>
-                      <br />
-                      <span>订单详情</span>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="operate">
-                      <span>查看发票</span>
-                      <br />
-                      <span>追加评论</span>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
               <tbody>
                 <tr class="tr-th">
                   <td colspan="5" class="td">
@@ -213,10 +145,6 @@
                     </div>
                   </div>
                   <div class="goods-number">x1</div>
-                  <!-- <div class="goods-repair">
-                      <span class="br">申请售后</span>
-                      <span class="br">卖了换钱</span>
-                    </div> -->
                 </td>
                 <td>
                   <div class="user">
@@ -256,7 +184,85 @@
               </tr>
             </tbody>
           </el-tab-pane>
-          <el-tab-pane label="待发货">Role</el-tab-pane>
+          <el-tab-pane label="待发货">
+            <el-table stripe style="width: 100%" empty-text=" ">
+                <el-table-column label="订单详情" width="570" />
+                <el-table-column label="收货人" width="100" />
+                <el-table-column label="金额" width="90" />
+                <el-table-column label="状态" width="90" />
+                <el-table-column label="操作" width="80" />
+              </el-table>
+
+            <tbody>
+                <tr class="tr-th">
+                  <td colspan="5" class="td">
+                    <span class="dealtime">2021-11-16 19:36:21</span>
+                    <span class="number">订单号:</span>
+                    <span class="numbers">230307184410</span>
+                    <span class="titless">京耳官方旗舰店</span>
+                    <img
+                      class="del"
+                      src="@/assets/images/icon-del.png"
+                      alt=""
+                    />
+                  </td>
+                </tr>
+                <tr class="tr-bd">
+                  <td>
+                    <div class="goods-item">
+                      <div class="p-img">
+                        <img src="@/assets/images/jer.jpg" alt="" />
+                      </div>
+                      <div class="p-msg">
+                        <div class="p-name">
+                          京耳
+                          蓝牙耳机真无线运动跑步游戏音乐降噪电脑双耳适用智能手机
+                          白色
+                        </div>
+                      </div>
+                    </div>
+                    <div class="goods-number">x1</div>
+                    <div class="goods-repair">
+                      <span class="br">退款/退货</span>
+                      <span class="br">投诉商家</span>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="user">
+                      <span class="txt">刘伟博</span>
+                      <img
+                        class="user-img"
+                        src="@/assets/images/icon-user.png"
+                        alt=""
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="amount">
+                      <span>￥9.90</span>
+                      <br />
+                      <span class="ftx-13">在线支付</span>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="status">
+                      <span class="amount">买家已付款</span>
+                      <br />
+                      <span>订单详情</span>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="operate">
+                      <span>修改订单</span>
+                      <br />
+                      <span>申请开票</span>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+
+            
+          </el-tab-pane>
           <el-tab-pane label="待收货">
             <el-table stripe style="width: 100%" empty-text=" ">
               <el-table-column label="订单详情" width="530" />
@@ -291,10 +297,6 @@
                     </div>
                   </div>
                   <div class="goods-number">x1</div>
-                  <!-- <div class="goods-repair">
-                      <span class="br">申请售后</span>
-                      <span class="br">卖了换钱</span>
-                    </div> -->
                 </td>
                 <td>
                   <div class="user">
@@ -335,7 +337,81 @@
             </tbody>
 
           </el-tab-pane>
-          <el-tab-pane label="待评价">Task</el-tab-pane>
+          <el-tab-pane label="待评价">
+            <el-table stripe style="width: 100%" empty-text=" ">
+              <el-table-column label="订单详情" width="530" />
+              <el-table-column label="收货人" width="80" />
+              <el-table-column label="金额" width="90" />
+              <el-table-column label="状态" width="90" />
+              <el-table-column label="操作" width="80" />
+            </el-table>
+
+            <tbody>
+              <tr class="tr-th">
+                <td colspan="5" class="td">
+                  <span class="dealtime">2021-11-16 19:36:21</span>
+                  <span class="number">订单号:</span>
+                  <span class="numbers">230307184410</span>
+                  <span class="titless">京耳官方旗舰店</span>
+                  <img class="del" src="@/assets/images/icon-del.png" alt="" />
+                </td>
+              </tr>
+              <tr class="tr-bd">
+                <td>
+                  <div class="goods-item">
+                    <div class="p-img">
+                      <img src="@/assets/images/jer.jpg" alt="" />
+                    </div>
+                    <div class="p-msg">
+                      <div class="p-name">
+                        京耳
+                        蓝牙耳机真无线运动跑步游戏音乐降噪电脑双耳适用智能手机
+                        白色
+                      </div>
+                    </div>
+                  </div>
+                  <div class="goods-number">x1</div>
+                </td>
+                <td>
+                  <div class="user">
+                    <span class="txt">刘伟博</span>
+                    <img
+                      class="user-img"
+                      src="@/assets/images/icon-user.png"
+                      alt=""
+                    />
+                  </div>
+                </td>
+                <td>
+                  <div class="amount">
+                    <span class="payment">应付</span>
+                    <br />
+                    <span class="payment">￥9.90</span>
+                    <br />
+                    <span class="ftx-13">在线支付</span>
+                  </div>
+                </td>
+                <td>
+                  <div class="status">
+                    <span>交易成功</span>
+                    <br />
+                    <span>订单详情</span>
+                    <br>
+                    <span class="origin">查看物流</span>
+                    <br>
+                    <span>对方已评</span>
+                  </div>
+                </td>
+                <td>
+                  <div class="operate">
+                    <span class="payment-pay">评价</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+
+
+          </el-tab-pane>
         </el-tabs>
       </div>
     </div>
