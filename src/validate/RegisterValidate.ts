b096623f7d193eb7438ddcab62398eb36f0d7e98
hyx1958895296 <1958895296@qq.com>
2022-11-28 11:05:17 +0800

    注册校验ok

--- src/validate/RegisterValidate.ts
+++ src/validate/RegisterValidate.ts
@@ -16,16 +16,22 @@ const rule = {
     phoneNumber: function (val: string) {
         return {
             isOk: val.length != 11,
-            msg: '密码必须为6-20位之间'
+            msg: '手机号必须为11位'
         }
-    }
+    },
+    avatarName: function (val: string) {
+        return {
+            isOk: val.length >= 1 && val.length <= 12,
+            msg: '昵称必须为1-12位之间'
+        }
+    },
 }
 
 export default class Registgers extends Validate<RegisterPayload> {
     constructor(payload: RegisterPayload) {
         super(payload);
     }
-    ruleConfig(): { phoneNumber: Function; username: Function; password: Function; } {
+    ruleConfig(): { phoneNumber: Function; username: Function; password: Function; avatarName:Function} {
         return rule
     }
 
