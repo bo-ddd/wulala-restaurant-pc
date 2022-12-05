ac3ec8126821c8cface9c88a1d2c20c074d78b87
zyfhup <3375926016@qq.com>
2022-12-05 20:40:08 +0800

    api添加类型规范

---
+++ src/types/xhrPayLoadApi.ts
@@ -0,0 +1,85 @@
+//登录
+export interface login{
+    username?:string,
+    password?:string,
+}
+//注册
+export interface register{
+    username?:string,	
+    password?:string,	
+    avatarName?:string,	
+    phoneNumber?:string,	
+}
+//查询用户信息
+export interface queryUserInfo{
+    userId?:number,
+}
+
+//获取菜肴列表接口
+export interface gatFoodList{
+    pageSize?:number,
+    pageNum?:number,
+    categoryId?:number,
+}
+//获取菜肴详情接口
+export interface gatFoodDetail{
+    foodId?:number,
+}
+//菜肴评价列表接口
+export interface foodAppraiseList{
+    foodId?:number,
+}
+//新增菜肴评价
+export interface addFoodAppraise{
+    userId?:number,
+    foodId?:number,
+    content?:string,
+    star?:number,
+    isRealName?:number,
+}
+//删除我的购物车
+export interface cartDelete{
+    id?:number,
+}
+//加入购物车
+export interface addShoppingCart{
+    id?:number,
+    productId?:number,
+    quantity?:number,
+}
+//新增收货地址
+export interface addressCreate{
+    provinceCode?:number,
+    cityCode?:number,
+    areaCode?:number,
+    address?:string,
+    townCode?:number,
+    isDefaultActive?:string,
+    phoneNumber?:string,
+    receiver?:string,
+}
+//订单列表
+export interface orderList{
+    userId?:number,
+}
+//删除收货地址
+export interface addressDelete{
+    id?:number,
+}
+//修改收货地址信息
+export interface addressUpdate{
+    id?:number,
+    provinceCode?:number,
+    cityCode?:number,
+    areaCode?:number,
+    address?:string,
+    townCode?:number,
+    isDefaultActive?:number,
+    phoneNumber?:string,
+    receiver?:string,
+}
+//创建订单
+export interface orderCreate{
+    addressId?:number,
+    rows:any
+}
